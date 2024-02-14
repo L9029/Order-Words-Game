67d31391ad91c06e70aaa7d87c3c4054dae753ee
L9029 <angeloca03@gmail.com>
2024-02-14 07:34:19 -0400

    Making little changes in the analysis function and the html file.

--- func/analisis.php
+++ func/analisis.php
@@ -7,7 +7,6 @@ function validation(){
     for ($i=0; $i < count($arr) ; $i++) { 
         if ($_REQUEST["$i"] != $arr[$i]) {
             echo "La palabra es incorrecta, la palabra es: " . $arr[$i] . "<br>";
-            echo "\n";
         }else {
             echo "La palabra es correcta" . "<br>"; //<br> Realiza un salto de línea
         }   
